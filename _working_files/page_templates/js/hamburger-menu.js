f437038f9cd1dbf09e4a4340b209320e3b3966bf
Michaela <michaela.tsumura@surrey.ca>
2017-02-25 14:34:28 -0800

    Adding a test hamburger menu to the mobile navigation

---
+++ _working_files/page_templates/js/hamburger-menu.js
@@ -0,0 +1,20 @@
+document.getElementById('hamburger').addEventListener('click', checkNav);
+  window.addEventListener("keyup", function(e) {
+    if (e.keyCode == 27) closeNav();
+  }, false);
+
+  function checkNav() {
+    if (document.body.classList.contains('hamburger-active')) {
+      closeNav();
+    } else {
+      openNav();
+    }
+  }
+
+  function closeNav() {
+    document.body.classList.remove('hamburger-active');
+  }
+
+  function openNav() {
+    document.body.classList.add('hamburger-active');
+  }
